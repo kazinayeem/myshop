b3f7ef12be21d087bb1dad09467b315822f7f9e5
kazinayeem <mohammadalinayeem360@gmail.com>
2025-02-14 11:29:29 +0600

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Navbar from "@/components/Navbar";
 import NavbarWrapper from "@/components/NavbarWrapper";
 
 const geistSans = Geist({
